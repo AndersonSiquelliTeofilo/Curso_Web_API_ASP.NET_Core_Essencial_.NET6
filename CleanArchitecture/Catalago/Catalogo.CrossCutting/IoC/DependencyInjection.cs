74c2e3214e21c9ffc3a3f77033bbabe16b55459e
AndersonSiquelliTeofilo <anderson.siquelli@gmail.com>
2022-06-29 20:35:16 -0300

    criação da cross cutting

---
+++ CleanArchitecture/Catalago/Catalogo.CrossCutting/IoC/DependencyInjection.cs
@@ -0,0 +1,30 @@
+﻿using Catalogo.Application.Interfaces;
+using Catalogo.Application.Mappings;
+using Catalogo.Application.Services;
+using Catalogo.Domain.Interfaces;
+using Catalogo.Infrastructure.Context;
+using Catalogo.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalogo.CrossCutting.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            return services;
+        }
+    }
+}
